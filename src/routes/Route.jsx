5a31df0cadc4aae3d9d1521340c84646cba5c83b
Shourav paul <paul.shourav01@gmail.com>
2024-01-30 01:14:41 +0600

    Error Page

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -2,21 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import AdminLayout from "../layouts/AdminLayout";
 import HomePage from "../pages/mainPages/Home/HomePage";
+import ErrorPage from "../pages/sharedPages/Errors/ErrorPage";
+
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout/>,
+      errorElement:<ErrorPage/>,
       children:[
         {
           path:'/',
-          element:<HomePage/>,
+          element:<HomePage/>
         },
       ],
     },
     {
         path: "/dashboard",
         element: <AdminLayout/>,
+        errorElement:<ErrorPage/>,
     },
   ]);
 
